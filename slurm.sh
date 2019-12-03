f887a36a01029c6239a95ea26c0177bc0de0f7db
cannin <cannin@gmail.com>
2019-12-03 09:51:17 -0500

    Updated .sh

--- slurm.sh
+++ slurm.sh
@@ -7,8 +7,8 @@
 #SBATCH --mem-per-gpu=8G
 #SBATCH --mail-user=augustin_luna@hms.harvard.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o log_%j.out
-#SBATCH -e log_%j.err
+#SBATCH -o %j.out
+#SBATCH -e %j.err
 
 module load gcc/6.2.0
 module load python/3.7.4
