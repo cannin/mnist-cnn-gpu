7d01b7d909f795ceac540c69b239204d29c39dda
cannin <cannin@gmail.com>
2019-12-14 10:46:00 -0500

    Update script

--- nvidia.sh
+++ nvidia.sh
@@ -10,4 +10,10 @@
 module load gcc/6.2.0
 module load cuda/10.0
 
+echo "#---------"
+echo $CUDA_VISIBLE_DEVICES
+nvidia-smi
+nvidia-smi |grep "|\    [$CUDA_VISIBLE_DEVICES] "|awk '{print $3}'|xargs -r ps -o pid,ppid,uid -p
+echo "#----------"
+
 ~/nvida_samples/1_Utilities/deviceQuery/deviceQuery
